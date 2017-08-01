db999c30f0fcd37bb1264268817c3b152047a278
Jakub Chmura <chmura.jakub@gmail.com>
2017-08-01 21:06:57 +0200

    Support other SMIv1 variations than ending with iso

--- src/pl/jakubchmura/snmp/mib/psi/impl/SmiV1MibNodeMixin.java
+++ src/pl/jakubchmura/snmp/mib/psi/impl/SmiV1MibNodeMixin.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pl.jakubchmura.snmp.mib.psi.SmiNameAndNumber;
 import pl.jakubchmura.snmp.mib.psi.SmiNameValueString;
+import pl.jakubchmura.snmp.mib.reference.MibNodeReference;
 
 public class SmiV1MibNodeMixin extends SmiMibNodeImpl {
 
@@ -35,9 +36,12 @@ public class SmiV1MibNodeMixin extends SmiMibNodeImpl {
             return new SmiV1MibNodeMixin((SmiNameAndNumber) prevSibling);
         } else if (prevSibling instanceof SmiNameValueString) {
             SmiNameValueString smiNameValueString = (SmiNameValueString) prevSibling;
-            String name = smiNameValueString.getName();
-            if (IsoMibNode.ISO.equals(name)) {
-                return new IsoMibNode(smiNameValueString.getNode());
+            MibNodeReference mibNodeReference = smiNameValueString.getReference();
+            if (mibNodeReference != null) {
+                PsiElement resolve = mibNodeReference.resolve();
+                if (resolve instanceof SmiMibNodeMixin) {
+                    return (SmiMibNodeMixin) resolve;
+                }
             }
         }
         return null;
