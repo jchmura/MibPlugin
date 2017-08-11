ac45b692968702fb85ca8712ab3ce7a82b284f2c
Jakub Chmura <chmura.jakub@gmail.com>
2017-08-11 11:15:34 +0200

    Check for null to prevent NPE inside Matcher

--- src/main/java/pl/jakubchmura/snmp/mib/reference/marker/GroovyMibNodeMarkerProvider.java
+++ src/main/java/pl/jakubchmura/snmp/mib/reference/marker/GroovyMibNodeMarkerProvider.java
@@ -15,11 +15,13 @@ public class GroovyMibNodeMarkerProvider extends LanguageSmiReferenceMarkerProvi
         if (element instanceof GrLiteral) {
             GrLiteral literal = (GrLiteral) element;
             String value = literal.getValue() instanceof String? (String) literal.getValue(): null;
-            Matcher matcher = getMatcher(value);
-            if (value != null && matcher.matches()) {
-                RelatedItemLineMarkerInfo<PsiElement> lineMarkerInfo = collectSmiReferences(element, matcher.group(1));
-                if (lineMarkerInfo != null) {
-                    result.add(lineMarkerInfo);
+            if (value != null) {
+                Matcher matcher = getMatcher(value);
+                if (matcher.matches()) {
+                    RelatedItemLineMarkerInfo<PsiElement> lineMarkerInfo = collectSmiReferences(element, matcher.group(1));
+                    if (lineMarkerInfo != null) {
+                        result.add(lineMarkerInfo);
+                    }
                 }
             }
         }
